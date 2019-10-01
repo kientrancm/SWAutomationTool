88a767f9fb6f986d259c4230b6a12482de33b41c
Kien (William) Tran <kientrancm@gmail.com>
2019-10-01 11:35:54 +0700

    Add files via upload

--- SABHandler/SABHandler.py
+++ SABHandler/SABHandler.py
@@ -1,51 +1,11 @@
-'''
-Automation Script - SAB Test case generation for GCAPE
-Date            Author      Version     Description
-26 Mar 2019     Kien Tran   3.1         Change behavior of SR for in and out => Change Get/Set
-
-'''
-
-# ------------------LIBRARY----------------------------
-import os, re
+import os
 import xlrd
 import openpyxl as excel
 from openpyxl import Workbook, load_workbook
-import tkinter
-from tkinter import *
-from tkinter import filedialog
-
-# ------------------INIT DATA--------------------------
-data_path = "C:/Data/SABHandlerGCAPE/Temp/SABInput.xlsx"
-pre_output_path = "C:/Data/SABHandlerGCAPE/Temp/PreOutput.xlsx"
-list_interface = "C:/Data/SABHandlerGCAPE/Lists_Report.xlsx"
-output_path = "C:/Data/SABHandlerGCAPE/"
-title_door = (
-"Component", "Object Text", "SAB Interface", "H_SafetyClassification", "TS_Module_Name", "TS_Test Case Status",
-"TS_Test Type", "TS_Test Priority", "TS_Object type", "TS_Precondition / Dependency", "TS_Test Description",
-"TS_Expected Result", "Return", "Interface Type")
-
-title_door_final = (
-"Component", "Object Text", "SAB Interface", "H_SafetyClassification", "TS_Module_Name", "TS_Test Case Status",
-"TS_Test Type", "TS_Test Priority", "TS_Object type", "TS_Precondition / Dependency", "TS_Test Description",
-"TS_Expected Result")
-
-f_Component = 1
-f_Object_Text = 2
-f_SAB_Interface = 3
-f_H_SafetyClassification = 4
-f_TS_Module_Name = 5
-f_TS_Test_Case_Status = 6
-f_TS_Test_Type = 7
-f_TS_Test_Priority = 8
-f_TS_Object_type = 9
-f_TS_Precondition_Dependency = 10
-f_TS_Test_Description = 11
-f_TS_Expected_Result = 12
-f_Return = 13
 
 
 # Ham chuyen doi file xls sang xlsx
-def cvt_xls_to_xlsx(src_file_path):
+def cvt_xls_to_xlsx(src_file_path, name_file_path):
     book_xls = xlrd.open_workbook(src_file_path)
     book_xlsx = Workbook()
 
@@ -62,11 +22,13 @@ def cvt_xls_to_xlsx(src_file_path):
             for col in range(0, sheet_xls.ncols):
                 sheet_xlsx.cell(row=row + 1, column=col + 1).value = sheet_xls.cell_value(row, col)
 
-    book_xlsx.save(src_file_path + "x")
+    book_xlsx.save(name_file_path + ".xlsx")
 
 
 # Initial Input data from SAB file
-def initDataInput(url):
+#input: xlsx file
+def initDataInput(url, name_file):
+    out_data_path = "_int_" + name_file + ".xlsx"
     #
     print("msg Checking file SAB path...")
     if os.path.exists(url):
@@ -89,7 +51,7 @@ def initDataInput(url):
 
         data_output_ws.delete_rows(1, 8)
         data_output_ws.delete_cols(1)
-        data_output_wb.save(data_path)
+        data_output_wb.save(out_data_path)
 
         # create AutoTypeDefinition sheet
         sab_ws = sab_wb['AutoTypeDefinition']
@@ -100,7 +62,7 @@ def initDataInput(url):
                 data_output_ws[cell.coordinate].value = cell.value
 
         data_output_ws.delete_rows(1, 10)
-        data_output_wb.save(data_path)
+        data_output_wb.save(out_data_path)
 
         sab_wb.close()
         data_output_wb.close()
@@ -110,11 +72,23 @@ def initDataInput(url):
     else:
         print("error File SAB not found")
 
+title_door = ("Component","Object Text","SAB Interface","H_SafetyClassification","TS_Module_Name","TS_Test Case Status","TS_Test Type","TS_Test Priority","TS_Object type","TS_Precondition / Dependency","TS_Test Description","TS_Expected Result", "Return", "Interface Type")
 
-# readDataInput
-def readDataInput():
-    print("Creating testcase...")
-    # load data
+f_Component = 1
+f_Object_Text = 2
+f_SAB_Interface = 3
+f_H_SafetyClassification = 4
+f_TS_Module_Name = 5
+f_TS_Test_Case_Status = 6
+f_TS_Test_Type = 7
+f_TS_Test_Priority = 8
+f_TS_Object_type = 9
+f_TS_Precondition_Dependency = 10
+f_TS_Test_Description = 11
+f_TS_Expected_Result = 12
+f_Return = 13
+
+def readDataInput(data_path, name_file):
     wb = load_workbook(data_path)
     data = wb['Interfaces']
     max_row = data.max_row
@@ -200,7 +174,7 @@ def readDataInput():
                 if data.cell(row=data_rows, column=5).value == "in":
                     # KT 26/03/2019
                     # Change Set to Get
-                    data_Object_Text = "Get_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(
+                    data_Object_Text = "SAB_Get_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(
                         row=data_rows, column=3).value
                     data_TS_Test_Description = "1. Run to where " + data.cell(row=data_rows,
                                                                               column=1).value + "_Main call required SAB" + "\r\n" + "2. Check " + data.cell(
@@ -211,7 +185,7 @@ def readDataInput():
                 elif data.cell(row=data_rows, column=5).value == "out":
                     # KT 26/03/2019
                     # Change Get to Set
-                    data_Object_Text = "Set_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(
+                    data_Object_Text = "SAB_Set_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(
                         row=data_rows, column=3).value
                     data_TS_Test_Description = "1. Run to where " + data.cell(row=data_rows,
                                                                               column=1).value + "_Main call required SAB" + "\r\n" + "2. Check " + data.cell(
@@ -221,14 +195,15 @@ def readDataInput():
                         row=data_rows, column=11).value + " equal to required values"
 
             elif data.cell(row=data_rows, column=4).value == "CS":
-                data_Object_Text = "Call_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(row=data_rows,
+                data_Object_Text = "SAB_Call_" + data.cell(row=data_rows, column=2).value + "_" + data.cell(row=data_rows,
                                                                                                         column=3).value
                 data_TS_Precondition_Dependency = "1. SW run normal" + "\r\n" + "2. Input all available values"
-                data_TS_Test_Description = "1. Set BP1 at " + data.cell(row=data_rows,
-                                                                        column=1).value + " in " + data.cell(
-                    row=data_rows, column=1).value + "\r\n" + "2. Set BP2 at " + data.cell(row=data_rows,
-                                                                                           column=3).value + " in " + data.cell(
-                    row=data_rows, column=2).value
+                data_TS_Test_Description = "1. Set BP1 at " + data.cell(row=data_rows, column=1).value + " in " + data.cell(row=data_rows, column=1).value + "\r\n" \
+                                           + "2. Set BP2 at " + data.cell(row=data_rows, column=3).value + " in " + data.cell(row=data_rows, column=2).value + "\r\n"\
+                                           + "3. When software halt at BP1 set value param1 & param2 " + "\r\n"\
+                                           + "4. Press 'Run until' check flow of software  " + "\r\n" \
+                                           + "5. After exit Mapping, function check data return  " + "\r\n"
+
                 data_TS_Expected_Result = "1. BP1 reached" + "\r\n" + "2. BP2 reached"
 
             elif data.cell(row=data_rows, column=4).value == "Message":
@@ -251,6 +226,11 @@ def readDataInput():
                     data_TS_Expected_Result = "1. SW halt at where " + data.cell(row=data_rows,
                                                                                  column=1).value + "_Main call required SAB" + "\r\n" + "2. " + data.cell(
                         row=data_rows, column=11).value + " equal to required values"
+            '''
+            elif data.cell(row=data_rows, column=4).value == "Event"
+                data_TS_Precondition_Dependency = "1. SW run normal \r\n 2. Input all available values"
+                if data.cell(row=data_rows, column=5).value == ""
+            '''
 
             # ----------------
             # 2.Object Text
@@ -281,7 +261,9 @@ def readDataInput():
 
             rows = rows + 1
 
+    list_interface = "_List_Report_" + name_file + ".xlsx"
     wb2.save(list_interface)
+    pre_output_path = "_preOut_" + name_file + ".xlsx"
     output.save(pre_output_path)
 
     data = wb['AutoTypeDefinition']
@@ -297,8 +279,11 @@ def readDataInput():
     print("Finished Create Test Case")
 
 
+
+title_door_final = ("Component","Object Text","SAB Interface","H_SafetyClassification","TS_Module_Name","TS_Test Case Status","TS_Test Type","TS_Test Priority","TS_Object type","TS_Precondition / Dependency","TS_Test Description","TS_Expected Result")
+
 # Xu ly du lieu voi Value_t refer to AutoTypeDefinition sheet
-def FinalOutput(name):
+def FinalOutput(pre_output_path, name_file):
     # Open PreOutput.xlsx
     data = load_workbook(pre_output_path)
     in_data = data['PreOutput']
@@ -344,22 +329,22 @@ def FinalOutput(name):
                 output.cell(row=out_row, column=12).value = in_data.cell(row=data_row, column=12).value + " (0)"
                 out_row = out_row + 1
             else:
-                type_defined = FALSE
-                range_defined = FALSE
+                type_defined = False
+                range_defined = False
                 list_value_range = []
                 count_value_range = 0
                 for type_row in range(2, max_row_type + 1):
                     # Type was defined in AutoTypeDefinition sheet
                     if in_type.cell(row=type_row, column=1).value == in_data.cell(row=data_row, column=13).value:
-                        type_defined = TRUE
+                        type_defined = True
                         if in_type.cell(row=type_row, column=4).value is not None:
                             # Range have value
-                            range_defined = TRUE
+                            range_defined = True
                             count_value_range = count_value_range + 1
                             list_value_range = list_value_range + [in_type.cell(row=type_row, column=4).value]
 
                 # Write test case
-                if (type_defined == FALSE) or (range_defined == FALSE):
+                if (type_defined == False) or (range_defined == False):
                     # Write test case to TestCaseOutput
                     # Test case with SR but the return type is not defined or not value of range.-> Will be create one test case with value (0)
                     output.cell(row=out_row, column=1).value = in_data.cell(row=data_row, column=1).value
@@ -377,13 +362,13 @@ def FinalOutput(name):
                     out_row = out_row + 1
 
                     # Write Type is not define to ListTypeNotDef
-                    if (type_defined == FALSE):
+                    if (type_defined == False):
                         list1.cell(row=out_row2, column=1).value = in_data.cell(row=data_row, column=13).value
                         out_row2 = out_row2 + 1
 
                 # Ghi test case SR voi kieu tra ve duoc dinh nghia va range duoc dinh nghia
                 # Tach cac gia tri cua range de tao test case.
-                if (type_defined == TRUE) and (range_defined == TRUE):
+                if (type_defined == True) and (range_defined == True):
                     if count_value_range == 1:
                         if (str(list_value_range[0]) == "0..255") or (str(list_value_range[0]) == "0...255"):
                             output.cell(row=out_row, column=1).value = in_data.cell(row=data_row, column=1).value
@@ -477,9 +462,9 @@ def FinalOutput(name):
                                 list_value_range[i]) + ")"
                             out_row = out_row + 1
 
-                elif (type_defined == TRUE) and (range_defined == FALSE):
+                elif (type_defined == True) and (range_defined == False):
                     # Write Type is define to ListTypeNotDef but not range
-                    if (range_defined == FALSE):
+                    if (range_defined == False):
                         list2.cell(row=out_row3, column=1).value = in_data.cell(row=data_row, column=13).value
                         out_row3 = out_row3 + 1
 
@@ -499,116 +484,40 @@ def FinalOutput(name):
             output.cell(row=out_row, column=12).value = in_data.cell(row=data_row, column=12).value
             out_row = out_row + 1
 
-    out.save(output_path + name + ".xlsx")
+    output_path = name_file + "_Test_Specification.xlsx"
+    out.save(output_path)
     out.close()
     data.close()
 
+def remove_files(path):
+    os.remove(path)
+    print("Removed file: ",path)
 
-# Ham de list ra danh sach cac type, require de submit cho developer
-def ListType(name):
-    input = load_workbook(output_path + name + ".xlsx")
-    list1 = input['ListTypeNotDef']
-    list2 = input['ListTypeNotRange']
+#Ham main xu ly moi thu
+def SABTestSpecGen(file_path, name_file_path):
+    _int_file_path = name_file_path + ".xlsx"
+    _int_data_path = "_int_" + name_file_path + ".xlsx"
+    _pre_output_path = "_preOut_" + name_file_path + ".xlsx"
+    _list_rp = "_List_Report_" + name_file_path + ".xlsx"
+    ##Xu ly du lieu dau vao
+    if file_path.endswith('.xls'):
+        print("End file is xls")
+        cvt_xls_to_xlsx(file_path, name_file_path)
+        initDataInput(_int_file_path, name_file_path)
 
-    output = load_workbook(list_interface)
-    out1 = output.create_sheet('ListTypeNotDef', 1)
-    out2 = output.create_sheet('ListTypeNotRange', 2)
+    elif file_path.endswith('.xlsx'):
+        print("End file is not xls, this is xlsx")
+        initDataInput(file_path, name_file_path)
 
-    values = []
-    for i in range(1, list1.max_row + 1):
-        if list1.cell(row=i, column=1).value in values:
-            pass  # if already in list do nothing
-        else:
-            values.append(list1.cell(row=i, column=1).value)
+    #Read data
+    readDataInput(_int_data_path, name_file_path)
+    FinalOutput(_pre_output_path, name_file_path)
 
-    for value in values:
-        out1.append([value])
+    #Tao report
 
-    values = []
-    for i in range(1, list2.max_row + 1):
-        if list2.cell(row=i, column=1).value in values:
-            pass  # if already in list do nothing
-        else:
-            values.append(list2.cell(row=i, column=1).value)
-
-    for value in values:
-        out2.append([value])
-
-    input.remove_sheet(list1)
-    input.remove_sheet(list2)
-    input.save(output_path + name + ".xlsx")
-    output.save(list_interface)
-    output.close()
-    input.close()
-
-
-# -----------------------------------------------------
-# GUI
-class SABHandlerGUI(tkinter.Frame):
-    def __init__(self, root):
-        tkinter.Frame.__init__(self, root)
-
-        self.winfo_toplevel().title("SABHandlerGCAPE - Version 3.1")
-
-        self.topLabelFrame = LabelFrame(self)
-        self.centerLabelFrame = LabelFrame(self)
-
-        self.outputNameValue = tkinter.StringVar()
-        self.outputNameValue.set('TestCaseOutputName')
-        self.StandardFormPathValue = tkinter.StringVar()
-        self.SABdataPathValue = tkinter.StringVar()
-        self.statusValue = StringVar()
-        self.statusValue.set('Automation Create SAB test case.')
-
-        self.reportNameLabel = Label(self.topLabelFrame, text='Output name').grid(row=1, column=1)
-        # self.formChooserButton = Button(self.topLabelFrame, text = 'Standard Form Path', command = self.openFileFormChooser).grid(row = 2, column = 1)
-        self.SABChooserButton = Button(self.topLabelFrame, text='SAB Data Path', command=self.openFileSABChooser).grid(
-            row=2, column=1)
-        self.reportNameEntry = Entry(self.topLabelFrame, width=40, bd=2, textvariable=self.outputNameValue).grid(row=1,
-                                                                                                                 column=2)
-        # self.FormPathEntry = Entry(self.topLabelFrame, width = 40, bd = 2, textvariable = self.StandardFormPathValue).grid(row = 2, column = 2)
-        self.SABPathEntry = Entry(self.topLabelFrame, width=40, bd=2, textvariable=self.SABdataPathValue).grid(row=2,
-                                                                                                               column=2)
-
-        self.generateButton = Button(self.centerLabelFrame, text='Generate TCs', command=self.generate).grid(row=1,
-                                                                                                             column=1,
-                                                                                                             columnspan=2)
-        self.statusLabel = Label(self.centerLabelFrame, textvariable=self.statusValue).grid(row=2, column=1,
-                                                                                            columnspan=2)
-
-        self.topLabelFrame.grid(row=1, column=1)
-        self.centerLabelFrame.grid(row=2, column=1)
-
-    def openFileSABChooser(self):
-        file_path = tkinter.filedialog.askopenfilename()
-        if str(file_path).endswith('.xls'):
-            self.SABdataPathValue.set(file_path)
-        elif str(file_path).endswith('.xlsx'):
-            self.SABdataPathValue.set(file_path)
-        else:
-            self.setStatus("Wrong file data format!")
-
-    def setStatus(self, status):
-        self.statusValue.set(status)
-
-    def generate(self):
-        if not str(self.outputNameValue.get()):
-            self.setStatus('Missing test case file name')
-        # elif not str(self.StandardFormPathValue.get()):
-        #    self.setStatus('Missing standart form path')
-        elif not str(self.SABdataPathValue.get()):
-            self.setStatus('Missing SAB data path')
-        else:
-            self.setStatus('Running....')
-            self.setStatus('Convert .xls to .xlsx')
-            cvt_xls_to_xlsx(str(self.SABdataPathValue.get()))
-            self.setStatus('Initial Input Data')
-            initDataInput(str(self.SABdataPathValue.get()) + "x")
-            self.setStatus('Read Data')
-            readDataInput()
-            self.setStatus('Write Test Case')
-            FinalOutput(str(self.outputNameValue.get()))
-            self.setStatus('Write List Type to send to Dev')
-            ListType(str(self.outputNameValue.get()))
-            self.setStatus('Finished')
-
+    #Xoa file tam
+    remove_files(_int_file_path)
+    remove_files(_int_data_path)
+    remove_files(_pre_output_path)
+    #Khi nao viet function tao report, thi xe update
+    remove_files(_list_rp)
